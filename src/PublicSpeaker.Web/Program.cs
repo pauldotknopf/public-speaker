900996cf3f07216392153fede76ca435b4387cb5
Paul Knopf <pauldotknopf@gmail.com>
2023-08-02 15:08:06 -0400

    Added http loging.

--- src/PublicSpeaker.Web/Program.cs
+++ src/PublicSpeaker.Web/Program.cs
@@ -30,6 +30,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 
+app.UseHttpLogging();
+
 app.UseStaticFiles();
 
 app.UseMiddleware<SpotifyAuthMiddleware>();
